a22b1b52008e318e53892775a5d07358bc2f231f
Luke Darling <luke@lukedarling.net>
2018-11-15 16:44:59 -0500

    Create floppy disk image during compilation

--- compile.bat
+++ compile.bat
@@ -5,4 +5,5 @@ if %snake_asm_log_size% gtr 0 echo There was an error compiling the kernel: & ty
 bin\nasm.exe -g -f bin -o bin\boot.bin src\boot.asm 2> log\compile.log
 for /f %%i in ("log\compile.log") do set snake_asm_log_size=%%~zi
 if %snake_asm_log_size% gtr 0 echo There was an error compiling the bootloader: & type compile.log & echo. & pause & exit
+bin\mkfloppy.exe bin\bootdisk.img bin\boot.bin bin\kernel.bin
 echo Compilation successful. > log\compile.log
