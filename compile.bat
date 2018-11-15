da5a004682dca3c840cf4c3bff2e565ba72ea7a3
Luke Darling <luke@lukedarling.net>
2018-11-15 16:40:11 -0500

    Forgot to compile bootloader

--- compile.bat
+++ compile.bat
@@ -1,5 +1,8 @@
 @echo off
-bin\nasm.exe -g -fbin -o bin\kernel.bin src\kernel.asm 2> log\compile.log
+bin\nasm.exe -g -f bin -o bin\kernel.bin src\kernel.asm 2> log\compile.log
 for /f %%i in ("log\compile.log") do set snake_asm_log_size=%%~zi
-if %snake_asm_log_size% gtr 0 echo There was an error compiling Snake-ASM: & type compile.log & echo. & pause & exit
+if %snake_asm_log_size% gtr 0 echo There was an error compiling the kernel: & type compile.log & echo. & pause & exit
+bin\nasm.exe -g -f bin -o bin\boot.bin src\boot.asm 2> log\compile.log
+for /f %%i in ("log\compile.log") do set snake_asm_log_size=%%~zi
+if %snake_asm_log_size% gtr 0 echo There was an error compiling the bootloader: & type compile.log & echo. & pause & exit
 echo Compilation successful. > log\compile.log
